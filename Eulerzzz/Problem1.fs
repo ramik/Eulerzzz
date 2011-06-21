326a2d2dfde6db7ddcd175386a11f701e8322d4b
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-06-22 00:25:03 +0300

    Problem 1 solution with unit tests

--- Eulerzzz/Problem1.fs
+++ Eulerzzz/Problem1.fs
@@ -3,8 +3,22 @@
 #r @"C:\Eulerzzz\lib\xunit.dll"
 #endif
 
-let a = 2
+let initSequence = Seq.initInfinite id |> Seq.filter (fun x -> x % 3 = 0 || x % 5 = 0)
+let takeItems max = Seq.takeWhile (fun x -> x < max) initSequence
 
 module problem1UnitTests = 
-    let b = 3
+  open Xunit
 
+  [<Fact>]
+  let sumsUpCorrectlyWhenBelow10() = 
+    let sum = takeItems 10 |> Seq.sum
+    Assert.Equal(23, sum)
+
+  [<Fact>]
+  let problemSolution() =
+    let sum = takeItems 1000 |> Seq.sum
+    Assert.Equal(233168, sum)
+
+module printSolution =
+  let sum = takeItems 1000 |> Seq.sum
+  printfn "Problem1 solution is: %i" sum
