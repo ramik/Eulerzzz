29772bb77962eb329bf32c187f4262a4e766d22c
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-06-26 14:53:44 +0300

    can find out palindrome tuples

--- Eulerzzz/Problem33.fs
+++ Eulerzzz/Problem33.fs
@@ -1,11 +1,22 @@
 ﻿module problem33
+#if INTERACTIVE
+#r @"C:\Eulerzzz\lib\xunit.dll"
+#endif
+
+open System
 
 let wouldResultZeroAfterSimplification x = match x with | (a, b) when a % 10 = 0 || b % 10 = 0 -> true
-                                                        | _ -> false           
+                                                        | _ -> false  
+                                                           
+let toReverseInt x = 
+    let toReverseIntInternal (y : string) = y.ToCharArray() |> Array.rev |> Array.fold (fun acc a -> String.Format("{0}{1}", acc, a)) ""
+    Int32.Parse(toReverseIntInternal (x.ToString()))
+
+let isPalindrome x = match x with | (a, b) when a = toReverseInt b -> true  
+                                  | _ -> false
+      
 module problem1UnitTests = 
-  #if INTERACTIVE
-  #r @"C:\Eulerzzz\lib\xunit.dll"
-  #endif
+
   open Xunit
 
   let trivialityCheckingOnTrivials =
@@ -16,12 +27,26 @@ module problem1UnitTests =
     let candidates = [(11, 22);(57, 75);(21, 44);(56, 65)]
     Assert.Equal(0, candidates |> Seq.filter wouldResultZeroAfterSimplification |> Seq.length)
 
+  let intReversing =
+    let candidates = [98; 11; 90; 75]
+    Assert.Equal([89; 11; 9; 57], (candidates |> List.map toReverseInt))
+
+  let isPalindromeCheckOnPalindromes =
+    let candidates = [(89, 98); (11, 11); (57, 75)]
+    Assert.Equal(3, Seq.filter isPalindrome candidates |> Seq.length)
+
+  let isPalindromeCheckOnNonPalindromes =
+    let candidates = [(89, 99); (11, 12); (55, 75)]
+    Assert.Equal(0, Seq.filter isPalindrome candidates |> Seq.length)
+
   [<Fact>] 
   let trivialityCheckingOnTrivialsGUI() = trivialityCheckingOnTrivials
 
   [<Fact>] 
   let trivialityCheckingOnNonTrivialsGUI() = trivialityCheckingOnNonTrivials
 
+  [<Fact>] 
+  let intReversingGUI() = intReversing
 
 module printSolution =
   printfn "%s" "not done"
