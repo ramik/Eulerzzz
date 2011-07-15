40d01f7f097f8f3b58d5ba30d512b0e9bf155997
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-07-15 19:06:44 +0300

    started solving problem 34

---
+++ Eulerzzz/Problem34.fs
@@ -0,0 +1,16 @@
+﻿module problem34
+#if INTERACTIVE
+#r @"C:\Eulerzzz\lib\xunit.dll"
+#endif
+
+open System
+open Microsoft.FSharp.Math
+
+let calculateFactorialSums x = x.ToString().ToCharArray() |> Seq.map (fun c -> Int32.Parse(c.ToString())) |> Seq.map (fun c -> (factorial c)) 
+calculateFactorialSums 145 |> ignore
+
+module problem34Unitests = 
+  open Xunit
+
+
+module printSolution = printfn "Solution's result is not done %A" [] 
