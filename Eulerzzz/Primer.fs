ffcc2e8be8681360a0b2f0021b7a0cf4e9b0aed1
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-12-24 16:07:50 +0200

    Prime internals to private, 2 and 3 are also included

--- Eulerzzz/Primer.fs
+++ Eulerzzz/Primer.fs
@@ -5,19 +5,23 @@
 #endif
 open System
 
-let isPrime x = let rec Sieve x y = match x % y, sqrt x < y with
+let private isPrime x = let rec Sieve x y = match x % y, sqrt x < y with
                                                  | (0.0, _) -> false           
                                                  | (_, true) -> true
                                                  | _ -> Sieve x (y + 2.0)
-                Sieve x 3.0
+                        Sieve x 3.0
 
-let Primes = seq { for i in 3.0 .. 2.0 .. System.Double.MaxValue do if(isPrime i) then yield i } 
+let private PrimesGreaterThan3 = seq { for i in 3.0 .. 2.0 .. System.Double.MaxValue do if(isPrime i) then yield i }
+let Primes = Seq.append {2.0..3.0} PrimesGreaterThan3
 
 module private PrimerUnitTests =
   open Xunit
 
   let ReturnsCorrect5items = 
-    Assert.Equal([5.0;7.0;11.0;13.0;17.0], Primes |> Seq.take 5 |> Seq.toList)
+    Assert.Equal([2.0;3.0;5.0;7.0;11.0;13.0;17.0], Primes |> Seq.take 7 |> Seq.toList)
  
-  let ReturnsCorrect5to10items = 
-    Assert.Equal([19.0;23.0;29.0;31.0;37.0], Primes |> Seq.skip 5 |> Seq.take 5 |> Seq.toList)
+  let ReturnsCorrect7to12items = 
+    Assert.Equal([19.0;23.0;29.0;31.0;37.0], Primes |> Seq.skip 7 |> Seq.take 5 |> Seq.toList)
+
+  let ReturnsCorrectWhenitems = 
+    Assert.Equal([2.0;3.0;5.0;7.0;11.0;13.0;17.0], Primes |> Seq.takeWhile (fun x -> x < 18.0) |> Seq.toList)
