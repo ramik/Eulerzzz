e46fa7436826335303838a02e48372f78903fc31
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-12-25 02:13:44 +0200

    Problem 36 solved

---
+++ Eulerzzz/Problem36.fs
@@ -0,0 +1,19 @@
+﻿module Problem36
+
+open System
+
+let convertToBinary (n:int) = Convert.ToString(n,2).ToCharArray() |> Array.toList
+
+let isPalindrome x = List.rev x = x
+let bothPalindrome (x, y) = match x, y with
+                              | (true, true) -> true
+                              | _ -> false  
+let is10BaseNumberPalindrome x = x.ToString().ToCharArray() |> Array.toList |> isPalindrome
+let is2BaseNumberPalindrome x = convertToBinary x |> isPalindrome
+
+let candidates = Seq.initInfinite (fun c -> c + 1)
+let result x = x |> Seq.map (fun c -> c, (is10BaseNumberPalindrome c, is2BaseNumberPalindrome c))
+                 |> Seq.filter (fun (_, c) -> bothPalindrome c)      
+
+let test = candidates |> Seq.takeWhile (fun c -> c < 1000000) |> result |> Seq.sumBy (fun (c, _) -> c) 
+
