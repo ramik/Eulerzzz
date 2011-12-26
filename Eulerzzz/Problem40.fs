563b66a3f85013e99be4c313df4a4ad104de4978
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-12-26 17:42:51 +0200

    Problem 40 solved

---
+++ Eulerzzz/Problem40.fs
@@ -0,0 +1,9 @@
+﻿module Problem40
+
+open System
+
+let a = Seq.initInfinite (fun c -> c + 1) |> Seq.takeWhile (fun c -> c < 200000) |> Seq.map (fun c -> c.ToString()) |> String.concat ""
+let b = a.ToCharArray()
+let nthItems = [0;9;99;999;9999;99999;999999]
+let result = nthItems |> Seq.map (fun c -> Seq.nth c b) |> Seq.map (fun c -> Int32.Parse (c.ToString())) |> Seq.fold (*) 1
+
