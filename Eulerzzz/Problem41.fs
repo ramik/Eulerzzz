eaef9eafda4f6b2cecefb2159abe4ef9ba245f0b
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-12-26 18:10:57 +0200

    Problem 41 solved

---
+++ Eulerzzz/Problem41.fs
@@ -0,0 +1,16 @@
+﻿module Problem41
+
+#if INTERACTIVE
+#r @"..\lib\xunit.dll"
+#load "Primer.fs"
+#endif
+
+open System 
+open Primer
+
+let isPandigital x = 
+      let arr = x.ToString().ToCharArray() |> Array.toList |> List.map (fun c -> Int32.Parse (c.ToString()))
+      let len = List.length arr
+      List.sort arr = [1..len]
+
+let result = Primes |> Seq.takeWhile (fun c -> c < 9876500.0) |> Seq.filter isPandigital |> Seq.max
