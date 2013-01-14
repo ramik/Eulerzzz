63960247dd9bf3f717411a9e9f66c5ea6692c34d
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2013-01-14 23:00:19 +0200

    Problem 46 solved

---
+++ Eulerzzz/Problem46.fs
@@ -0,0 +1,28 @@
+﻿module Problem46
+
+#if INTERACTIVE
+#r @"..\lib\xunit.dll"
+#load "Primer.fs"
+#endif
+
+open Primer
+
+let rec canBeFormed goldbach prime x = 
+     if(prime < 1.0) then false
+     else if(isPrime(prime) = false) then canBeFormed goldbach (prime - 2.0) x
+     else 
+        match prime + 2.0 * (x * x) with
+        | a when a = goldbach -> true
+        | a when a > goldbach -> canBeFormed goldbach (prime - 2.0) 1.0
+        | _ -> canBeFormed goldbach prime (x+1.0)
+ 
+let rec findGoldbach x result = 
+    if(result > 0.0) then result
+    else if(isPrime(x)) then findGoldbach (x + 2.0) result
+    else 
+        let resu = canBeFormed x (x - 2.0) 1.0
+        if(resu = false) then findGoldbach x x
+        else
+            findGoldbach (x + 2.0) result
+
+let result = findGoldbach 3.0 0.0
