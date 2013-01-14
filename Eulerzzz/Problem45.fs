743cb2efe2ccbb181b1d6888c425ea21a0bf23b2
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2013-01-14 21:46:22 +0200

    problem 45 solved

---
+++ Eulerzzz/Problem45.fs
@@ -0,0 +1,17 @@
+﻿module Problem45
+
+let triangle x = (x * (x + 1L)) / 2L
+let pentagonal x = (x * (3L * x - 1L)) / 2L
+let hexagonal x = x * (2L * x - 1L)
+
+let rec pairings x y z =
+    let hexa = hexagonal x
+    let penta = pentagonal y
+    let tri = triangle z
+    match hexa, penta, tri with 
+    | a, b, c when a = b && b = c -> a
+    | a, b, c when a < b || b < c -> pairings (x+1L) (x+1L) (x+1L)
+    | a, b, c when a = b && b > c -> pairings x y (z+1L)
+    | _ -> pairings x (y+1L) z
+
+let result = pairings 144L 144L 144L
