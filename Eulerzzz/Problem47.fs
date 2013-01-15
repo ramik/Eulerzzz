9ab518a1ff5f9874571352aa10d92afd2078bc9c
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2013-01-15 23:33:11 +0200

    problem 47 takes now 10s to solve

--- Eulerzzz/Problem47.fs
+++ Eulerzzz/Problem47.fs
@@ -7,34 +7,36 @@
 
 open Primer
 open System
-    
+
+let rec getNextPrime x = 
+    match x with
+    | 1.0 -> 2.0
+    | 2.0 -> 3.0
+    | _ -> let candidate = x + 2.0
+           if(isPrime candidate) then candidate
+           else
+                getNextPrime candidate   
+
 let findFactors candidate =
-    let rec findFactorsF prime result = 
-        if(isPrime(prime) = false) then findFactorsF (prime + 2.0) result
-        else
-            match prime, candidate with 
-            | 2.0, a when a % 2.0 = 0.0 -> findFactorsF 3.0 (2.0 :: result)
-            | 2.0, _ -> findFactorsF 3.0 result
-            | a, b when a > b / 2.0 -> result
-            | a, b when b % a = 0.0 -> findFactorsF (candidate / prime) (a :: result)
-            | _ -> findFactorsF (prime + 2.0) result
-    findFactorsF 2.0 []
+    let rec findFactorsF x primeStub result = 
+       if(isPrime x && x > primeStub) then (x :: result)
+       else
+           let prime = getNextPrime primeStub
+           match prime > x, x % prime with
+           | true, _ -> result
+           | _, 0.0 -> findFactorsF (x / prime) prime (prime :: result)
+           | _ -> findFactorsF x prime result
+    if(isPrime(candidate)) then []
+    else
+        findFactorsF candidate 1.0 []
 
 let findResult =
     let rec findResultF count result x =
         let factors = findFactors x
-        if(x % 1000.0 = 0.0) then printfn "%A %A %A" count result x 
         match Seq.length factors, count with
-        | 3, 3 -> result
-        | 3, 1 -> findResultF (count + 1) x (x + 1.0)
-        | 3, _ -> findResultF (count + 1) result (x + 1.0)
+        | 4, 4 -> result
+        | 4, 1 -> findResultF (count + 1) x (x + 1.0)
+        | 4, _ -> findResultF (count + 1) result (x + 1.0)
         | _, _ -> findResultF 1 0.0 (x + 1.0)
 
     findResultF 1 0.0 10.0
-
-// 14 = 2 x 7
-// 15 = 3 X 5
-
-// 644 = 2² × 7 × 23
-// 645 = 3 × 5 × 43
-// 646 = 2 × 17 × 19
