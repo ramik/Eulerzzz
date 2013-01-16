734f647de243d2ff511400fece98fb51da5e7786
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2013-01-16 08:49:42 +0200

    problem 48 solved

---
+++ Eulerzzz/Problem48.fs
@@ -0,0 +1,20 @@
+﻿module Problem48
+
+open System
+open System.Numerics
+
+let findResult howmany = 
+    let rec findResultF number exp result = 
+        if(exp > howmany) then result
+        else
+            let power = BigInteger.Pow(number, exp)
+            findResultF (number + 1I) (exp + 1) (result + power)
+    
+    findResultF 1I 1 0I
+
+let findResultStr = findResult 1000 |> (fun c -> c.ToString())
+let result = 
+    let resultStr = findResultStr.ToCharArray()
+    let len = resultStr |> Seq.length
+    resultStr |> Seq.skip (len - 10) |> (fun c -> String.Join("", c))
+                
