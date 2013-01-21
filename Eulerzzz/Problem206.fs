bdbfaef939a6bd8a5725cdd7e6806bceae29ab9f
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2013-01-21 14:02:13 +0200

    problem 206 solved

---
+++ Eulerzzz/Problem206.fs
@@ -0,0 +1,15 @@
+﻿module Problem206
+
+open System
+open System.Numerics
+open System.Text.RegularExpressions
+
+let start = 1414213562I
+let pattern = "1.2.3.4.5.6.7.8.9.0"
+
+let rec findSolution x = 
+    if(x % 10000000I = 0I) then printfn "%A" x
+    let candidate = BigInteger.Pow(x, 2)
+    match Regex.Match(candidate.ToString(), pattern) with
+    | m when m.Success -> x
+    | _ -> findSolution (x - 1I)
